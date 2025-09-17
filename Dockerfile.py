89b469f4a199a3b504adb20365086cffe216f343
jennifersteeden <jennifer.steeden@ucl.ac.uk>
2025-09-17 13:30:01 +0100

    I think that this creates different docker images for the each of the python versions

---
+++ Dockerfile.py
@@ -0,0 +1,64 @@
+FROM ghcr.io/mrphys/tensorflow-manylinux-base:latest
+
+ARG PY_VERSION=3.9 
+#3.10 3.11 3.12 3.13 3.14 3.15"
+RUN echo "hello there: ${PY_VERSION}"
+
+
+
+
+
+
+# Use only the requested Python version
+ENV PATH="/opt/python/cp${PY_VERSION/./}-cp${PY_VERSION/./}/bin:${PATH}"
+ENV PYBIN="/opt/python/cp${PY_VERSION/./}-cp${PY_VERSION/./}/bin/python"
+
+RUN echo "hello there, PATH: ${PATH}"
+
+# Upgrade pip & install TensorFlow
+# https://www.tensorflow.org/install/source
+# I only see 2.16 on the tensorflow website...?
+ARG TF_VERSION=2.19
+
+RUN ${PYBIN} -m pip install --upgrade pip setuptools wheel
+
+RUN ${PYBIN} -m pip install tensorflow==${TF_VERSION}
+
+# -------------------------------------------------
+# Python setup
+# -------------------------------------------------
+# manylinux ships with multiple Python versions in /opt/python
+# Example: cp38-cp38, cp39-cp39, cp310-cp310, cp311-cp311, cp312-cp312
+# Pick one version (default: 3.10)
+
+
+# Install TensorFlow on all supported Python versions.
+RUN for PYVER in ${PY_VERSION}; do ${PYBIN}${PYVER} -m pip install tensorflow==${TF_VERSION}; done
+
+# -------------------------------------------------
+# Sphinx & Python doc dependencies
+# -------------------------------------------------
+# was SPHINX_VERSION="4.5.0"
+ARG SPHINX_VERSION="5.3.0"
+# PYDATA_SPHINX_THEME_VERSION="0.8.0"
+ARG PYDATA_SPHINX_THEME_VERSION="0.14.4"
+# SPHINX_BOOK_THEME_VERSION="0.3.3"
+ARG SPHINX_BOOK_THEME_VERSION="1.0.1"
+# NEW
+ARG MYST_VERSION="0.17.2"
+
+ARG PYTHON_DEPS="sphinx==${SPHINX_VERSION} pydata-sphinx-theme==${PYDATA_SPHINX_THEME_VERSION} ipython sphinx-sitemap myst-nb==${MYST_VERSION} sphinx-book-theme==${SPHINX_BOOK_THEME_VERSION} pydot"
+
+RUN ${PYBIN} -m pip install ${PYTHON_DEPS}
+
+# -------------------------------------------------
+# Auditwheel
+# -------------------------------------------------
+RUN ${PYBIN} -m pip install --upgrade pip setuptools wheel auditwheel
+
+COPY patch_auditwheel.sh .
+
+RUN ./patch_auditwheel.sh "/opt/python/cp${PY_VERSION/./}-cp${PY_VERSION/./}/lib/python3.*"
+
+COPY extensions /opt/sphinx/extensions
+
---
+++ build_bash.sh
@@ -0,0 +1,16 @@
+# Run like this in terminal ./build_bash.sh
+
+# Build base image
+docker build -t ghcr.io/mrphys/tensorflow-manylinux-base:latest -f Dockerfile.base .
+
+# Build for each Python version
+# 
+for PY in 3.9 3.10 3.11 3.12; do
+    docker build \
+      --build-arg PY_VERSION=$PY \
+      -t ghcr.io/mrphys/tensorflow-manylinux:py$PY \
+      -f Dockerfile.py .
+    docker push ghcr.io/mrphys/tensorflow-manylinux:py$PY
+done
+
+
